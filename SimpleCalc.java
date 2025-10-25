3c549ff7996b4e08b83f61e349744ecc185132f8
Gaurav Singh <kanwasigaurav96@gmail.com>
2025-10-25 22:52:11 +0530

    Create SimpleCalc.java

---
+++ SimpleCalc.java
@@ -0,0 +1,11 @@
+public class SimpleCalc {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        
+        System.out.println("The sum is: " + sum);
+        System.out.println("The difference is: " + difference);
+    }
+}
