c3b5b0033be90c30893dc5c1576a729accd20bf4
Gaurav <kanwasigaurav96@gmail.com>
2025-11-12 22:45:04 +0530

    Add Java program using ternary operator

---
+++ TernaryExample.java
@@ -0,0 +1,8 @@
+public class TernaryExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int max = (a > b) ? a : b;
+        System.out.println("The larger number is " + max);
+    }
+}
