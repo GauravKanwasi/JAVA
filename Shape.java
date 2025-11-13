647ec2c5fdcd3b4b283c352ab5399c500480db1c
Gaurav Singh <kanwasigaurav96@gmail.com>
2025-11-13 22:13:34 +0530

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,14 @@
+public abstract class Shape {
+
+    protected String color;
+
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    public abstract double calculateArea();
+
+    public void displayColor() {
+        System.out.println("Color: " + color);
+    }
+}
