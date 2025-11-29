80197835db9097eae0bed89230c2718889f8a128
Gaurav Singh <kanwasigaurav96@gmail.com>
2025-11-29 22:41:28 +0530

    Update NestedLoopPattern.java

--- NestedLoopPattern.java
+++ NestedLoopPattern.java
@@ -1,11 +1,16 @@
-public class NestedLoopPattern {
+public class NumberPyramidPattern {
     public static void main(String[] args) {
-        int rows = 5;
-        int columns = 10;
+        int n = 5; 
 
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= columns; j++) {
-                System.out.print("* ");
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+
+            for (int k = 1; k <= i; k++) {
+                System.out.print(i + " ");
             }
             System.out.println();
         }
