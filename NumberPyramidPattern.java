36893a7c75142a5d643d1d7396a6a76f0dc81fd7
Gaurav <kanwasigaurav96@gmail.com>
2025-11-30 10:00:00 +0530

    Completed the number pattern feature

--- NumberPyramidPattern.java
+++ NumberPyramidPattern.java
@@ -1,17 +1,19 @@
 public class NumberPyramidPattern {
     public static void main(String[] args) {
         int n = 5; 
+        int number = 1; 
 
         for (int i = 1; i <= n; i++) {
 
             for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
+                System.out.print("  "); 
             }
-
 
             for (int k = 1; k <= i; k++) {
-                System.out.print(i + " ");
+                System.out.printf("%3d", number); 
+                number++; 
             }
+            
             System.out.println();
         }
     }
