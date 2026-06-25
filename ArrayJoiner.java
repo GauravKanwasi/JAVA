9ec1d8849356477c43dd51f6ddbf40bbe045c1c1
Gaurav Singh <kanwasigaurav96@gmail.com>
2026-06-25 23:01:54 +0530

    Refactor array joining logic into a separate method

--- ArrayJoiner.java
+++ ArrayJoiner.java
@@ -1,32 +1,21 @@
-class ArrayJoiner {
+import java.util.Arrays;
+
+public class ArrayJoiner {
     public static void main(String[] args) {
         int[] firstArray = {10, 20, 30};
         int[] secondArray = {40, 50, 60, 70};
 
-        int firstLength = firstArray.length;
-        int secondLength = secondArray.length;
-        int resultLength = firstLength + secondLength;
+        int[] joinedArray = joinArrays(firstArray, secondArray);
+        
+        System.out.println("Joined Array: " + Arrays.toString(joinedArray));
+    }
 
-        int[] joinedArray = new int[resultLength];
-
-        int index = 0;
-        for (int element : firstArray) {
-            joinedArray[index] = element;
-            index++;
-        }
-
-        for (int element : secondArray) {
-            joinedArray[index] = element;
-            index++;
-        }
-
-        System.out.print("Joined Array: [");
-        for (int i = 0; i < joinedArray.length; i++) {
-            System.out.print(joinedArray[i]);
-            if (i < joinedArray.length - 1) {
-                System.out.print(", ");
-            }
-        }
-        System.out.println("]");
+    public static int[] joinArrays(int[] a, int[] b) {
+        int[] result = new int[a.length + b.length];
+        
+        System.arraycopy(a, 0, result, 0, a.length);
+        System.arraycopy(b, 0, result, a.length, b.length);
+        
+        return result;
     }
 }
