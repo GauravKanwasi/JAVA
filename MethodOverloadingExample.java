2a903c9835938ac2a3c738e78a6876c1aaf0c575
Gaurav Singh <kanwasigaurav96@gmail.com>
2026-06-26 22:20:39 +0530

    Change display methods to static
    Updated display methods to be static and modified calls in main.

--- MethodOverloadingExample.java
+++ MethodOverloadingExample.java
@@ -1,27 +1,21 @@
 public class MethodOverloadingExample {
 
-    public void display(int a) {
+    public static void display(int a) {
         System.out.println("Displaying integer: " + a);
     }
-
-    public void display(String s) {
+    public static void display(String s) {
         System.out.println("Displaying string: " + s);
     }
-
-    public void display(double d) {
+    public static void display(double d) {
         System.out.println("Displaying double: " + d);
     }
-
-    public void display(int a, String s) {
+    public static void display(int a, String s) {
         System.out.println("Displaying int and string: " + a + " and " + s);
     }
     public static void main(String[] args) {
-
-        MethodOverloadingExample obj = new MethodOverloadingExample();
-
-        obj.display(10);             
-        obj.display("Hello Java"); 
-        obj.display(25.5);
-        obj.display(100, "World");
+        display(10);
+        display("Hello Java");
+        display(25.5);
+        display(100, "World");
     }
 }
