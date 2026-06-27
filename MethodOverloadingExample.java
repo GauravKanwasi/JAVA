c8e0804bdd79fcff71f24d5f0d0a0ab1b763ff52
Gaurav Singh <kanwasigaurav96@gmail.com>
2026-06-27 19:09:27 +0530

    Refactor display methods for method overloading

--- MethodOverloadingExample.java
+++ MethodOverloadingExample.java
@@ -1,21 +1,48 @@
 public class MethodOverloadingExample {
 
-    public static void display(int a) {
-        System.out.println("Displaying integer: " + a);
+    public static void display(int number) {
+        System.out.println("Integer Value : " + number);
     }
-    public static void display(String s) {
-        System.out.println("Displaying string: " + s);
+
+    public static void display(double number) {
+        System.out.println("Double Value  : " + number);
     }
-    public static void display(double d) {
-        System.out.println("Displaying double: " + d);
+
+    public static void display(String text) {
+        System.out.println("String Value  : " + text);
     }
-    public static void display(int a, String s) {
-        System.out.println("Displaying int and string: " + a + " and " + s);
+
+    public static void display(char character) {
+        System.out.println("Character     : " + character);
     }
+
+    public static void display(int number, String text) {
+        System.out.println("Integer + String : " + number + ", " + text);
+    }
+
+    public static void display(String text, int number) {
+        System.out.println("String + Integer : " + text + ", " + number);
+    }
+
+    public static void display(int first, int second) {
+        System.out.println("Sum of Integers  : " + (first + second));
+    }
+
+    public static void display(int a, int b, int c) {
+        System.out.println("Average          : " + ((a + b + c) / 3.0));
+    }
+
     public static void main(String[] args) {
+
+        System.out.println("=== Method Overloading Demonstration ===\n");
+
         display(10);
-        display("Hello Java");
         display(25.5);
+        display("Hello Java");
+        display('A');
         display(100, "World");
+        display("Java", 2025);
+        display(15, 25);
+        display(10, 20, 30);
     }
 }
