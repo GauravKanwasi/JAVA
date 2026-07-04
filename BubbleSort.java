1fcb6fb0ff0d61b366128b0f74e56959bd4db996
Gaurav Singh <kanwasigaurav96@gmail.com>
2026-07-04 22:18:08 +0530

    Enhance BubbleSort to support order parameter
    Refactored BubbleSort to support sorting in both ascending and descending order using an enum. Updated main method to demonstrate sorting functionality.

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,32 +2,64 @@ import java.util.Arrays;
 
 public class BubbleSort {
 
+    public enum Order {
+        ASCENDING,
+        DESCENDING
+    }
+
     public void sort(int[] arr) {
+        sort(arr, Order.ASCENDING);
+    }
+
+    public void sort(int[] arr, Order order) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
         int n = arr.length;
-        boolean swapped;
 
         for (int i = 0; i < n - 1; i++) {
-            swapped = false;
+            boolean swapped = false;
+
             for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                boolean shouldSwap = order == Order.ASCENDING
+                        ? arr[j] > arr[j + 1]
+                        : arr[j] < arr[j + 1];
+
+                if (shouldSwap) {
+                    swap(arr, j, j + 1);
                     swapped = true;
                 }
             }
+
             if (!swapped) {
                 break;
             }
         }
     }
 
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void printArray(String message, int[] arr) {
+        System.out.println(message + Arrays.toString(arr));
+    }
+
     public static void main(String[] args) {
         BubbleSort sorter = new BubbleSort();
-        int[] data = { 64, 34, 25, 12, 22, 11, 90 };
 
-        System.out.println("Unsorted array: " + Arrays.toString(data));
-        sorter.sort(data);
-        System.out.println("Sorted array: " + Arrays.toString(data));
+        int[] ascending = {64, 34, 25, 12, 22, 11, 90};
+        int[] descending = ascending.clone();
+
+        printArray("Original: ", ascending);
+
+        sorter.sort(ascending);
+        printArray("Ascending: ", ascending);
+
+        sorter.sort(descending, Order.DESCENDING);
+        printArray("Descending: ", descending);
     }
 }
