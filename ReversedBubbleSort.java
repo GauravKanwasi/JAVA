59f59e54832451047f32471ccc865c3058d01f4e
Gaurav Singh <kanwasigaurav96@gmail.com>
2026-07-10 22:49:03 +0530

    Refactor bubble sort with swap method and print

--- ReversedBubbleSort.java
+++ ReversedBubbleSort.java
@@ -1,22 +1,46 @@
 public class ReversedBubbleSort {
     public static void bubbleSortDescending(int[] arr) {
         int n = arr.length;
+        boolean swapped;
+
         for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] < arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                    swap(arr, j, j + 1);
+                    swapped = true;
                 }
+            }
+
+            if (!swapped) {
+                break;
             }
         }
     }
 
-    public static void main(String[] args) {
-        int[] arr = {5, 1, 4, 2, 8};
-        bubbleSortDescending(arr);
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private static void printArray(int[] arr) {
         for (int num : arr) {
             System.out.print(num + " ");
         }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 4, 2, 8};
+
+        System.out.print("Original Array: ");
+        printArray(arr);
+
+        bubbleSortDescending(arr);
+
+        System.out.print("Sorted Array (Descending): ");
+        printArray(arr);
     }
 }
