b257fb67e3f08fb71d365ff322d12367ff36431a
Gaurav Singh <kanwasigaurav96@gmail.com>
2026-07-13 22:48:34 +0530

    Refactor VowelConsonantCounter for better analysis

--- VowelConsonantCounter.java
+++ VowelConsonantCounter.java
@@ -3,28 +3,44 @@ import java.util.Scanner;
 public class VowelConsonantCounter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a string:");
-        String inputString = scanner.nextLine();
-        scanner.close();
 
-        int vowelCount = 0;
-        int consonantCount = 0;
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
 
-        String lowerCaseString = inputString.toLowerCase();
+        int vowels = 0;
+        int consonants = 0;
+        int digits = 0;
+        int spaces = 0;
+        int specialChars = 0;
 
-        for (int i = 0; i < lowerCaseString.length(); i++) {
-            char ch = lowerCaseString.charAt(i);
+        for (char ch : input.toCharArray()) {
+            if (Character.isLetter(ch)) {
+                char c = Character.toLowerCase(ch);
 
-            if (ch >= 'a' && ch <= 'z') {
-                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                    vowelCount++;
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                    vowels++;
                 } else {
-                    consonantCount++;
+                    consonants++;
                 }
+            } else if (Character.isDigit(ch)) {
+                digits++;
+            } else if (Character.isWhitespace(ch)) {
+                spaces++;
+            } else {
+                specialChars++;
             }
         }
 
-        System.out.println("Vowels: " + vowelCount);
-        System.out.println("Consonants: " + consonantCount);
+        System.out.println("\n----- Analysis -----");
+        System.out.println("Input               : " + input);
+        System.out.println("Total Characters    : " + input.length());
+        System.out.println("Alphabets           : " + (vowels + consonants));
+        System.out.println("Vowels              : " + vowels);
+        System.out.println("Consonants          : " + consonants);
+        System.out.println("Digits              : " + digits);
+        System.out.println("Spaces              : " + spaces);
+        System.out.println("Special Characters  : " + specialChars);
+
+        scanner.close();
     }
 }
