9f9455c4f910cb7d95169834f1d1c6821650c2d6
Gaurav Singh <kanwasigaurav96@gmail.com>
2026-07-15 23:04:16 +0530

    Refactor bubble sort for optimization and clarity

--- OptimizedBubbleSort.java
+++ OptimizedBubbleSort.java
@@ -1,50 +1,59 @@
+import java.util.Arrays;
+
 public class OptimizedBubbleSort {
 
     public static void bubbleSort(int[] arr) {
         int n = arr.length;
-        boolean swapped;
 
-        for (int i = 0; i < n - 1; i++) {
-            swapped = false;
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                    swapped = true;
+        while (n > 1) {
+            int lastSwap = 0;
+
+            for (int i = 1; i < n; i++) {
+                if (arr[i - 1] > arr[i]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i - 1];
+                    arr[i - 1] = temp;
+                    lastSwap = i;
                 }
             }
 
-            if (!swapped) {
+            if (lastSwap == 0) {
                 break;
             }
+
+            n = lastSwap;
         }
     }
 
     public static void printArray(int[] arr) {
-        for (int element : arr) {
-            System.out.print(element + " ");
-        }
-        System.out.println();
+        System.out.println(Arrays.toString(arr));
     }
 
     public static void main(String[] args) {
         int[] data1 = {64, 34, 25, 12, 22, 11, 90};
-        System.out.println("Original array 1:");
-        printArray(data1);
-
-        bubbleSort(data1);
-        System.out.println("Sorted array 1:");
-        printArray(data1);
-        
-        System.out.println("--------------------");
-
         int[] data2 = {10, 20, 30, 40, 50};
-        System.out.println("Original array 2:");
+        int[] data3 = {5, 1, 4, 2, 8, 0, 2};
+
+        System.out.println("Original: ");
+        printArray(data1);
+        bubbleSort(data1);
+        System.out.println("Sorted: ");
+        printArray(data1);
+
+        System.out.println();
+
+        System.out.println("Original: ");
+        printArray(data2);
+        bubbleSort(data2);
+        System.out.println("Sorted: ");
         printArray(data2);
 
-        bubbleSort(data2);
-        System.out.println("Sorted array 2:");
-        printArray(data2);
+        System.out.println();
+
+        System.out.println("Original: ");
+        printArray(data3);
+        bubbleSort(data3);
+        System.out.println("Sorted: ");
+        printArray(data3);
     }
 }
