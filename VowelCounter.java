6e05548ef1f7956841d78fd7ec95194333d609aa
Gaurav Singh <kanwasigaurav96@gmail.com>
2026-07-18 22:19:37 +0530

    Refactor vowel counting logic in VowelCounter

--- VowelCounter.java
+++ VowelCounter.java
@@ -1,33 +1,36 @@
 public class VowelCounter {
 
+    private static final String VOWELS = "aeiou";
+
     public static int countVowels(String str) {
-        if (str == null || str.isEmpty()) {
+        if (str == null || str.isBlank()) {
             return 0;
         }
 
-        int vowelCount = 0;
-        String lowerStr = str.toLowerCase();
+        int count = 0;
 
-        for (int i = 0; i < lowerStr.length(); i++) {
-            char ch = lowerStr.charAt(i);
-
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
-                vowelCount++;
+        for (char ch : str.toLowerCase().toCharArray()) {
+            if (VOWELS.indexOf(ch) != -1) {
+                count++;
             }
         }
 
-        return vowelCount;
+        return count;
     }
 
     public static void main(String[] args) {
-        String testString1 = "Hello World";
-        String testString2 = "Programming is Fun";
-        String testString3 = "AEIOUaeiou";
-        String testString4 = "Rhythm";
+        String[] testStrings = {
+            "Hello World",
+            "Programming is Fun",
+            "AEIOUaeiou",
+            "Rhythm",
+            "",
+            null
+        };
 
-        System.out.println("Vowels in \"" + testString1 + "\": " + countVowels(testString1));
-        System.out.println("Vowels in \"" + testString2 + "\": " + countVowels(testString2));
-        System.out.println("Vowels in \"" + testString3 + "\": " + countVowels(testString3));
-        System.out.println("Vowels in \"" + testString4 + "\": " + countVowels(testString4));
+        for (String str : testStrings) {
+            System.out.printf("Vowels in %-20s : %d%n",
+                    String.valueOf(str), countVowels(str));
+        }
     }
 }
