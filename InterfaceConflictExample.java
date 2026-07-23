28a138879b08d914ed929697c2976204483259ad
Gaurav Singh <kanwasigaurav96@gmail.com>
2026-07-23 22:35:26 +0530

    Resolve interface method conflict in Test class
    Updated the Test class to resolve method conflicts and demonstrate interface method calls.

--- InterfaceConflictExample.java
+++ InterfaceConflictExample.java
@@ -2,18 +2,34 @@ interface A {
     default void message() {
         System.out.println("Message from Interface A");
     }
+
+    default void greet() {
+        System.out.println("Hello from Interface A");
+    }
 }
 
 interface B {
     default void message() {
         System.out.println("Message from Interface B");
     }
+
+    default void goodbye() {
+        System.out.println("Goodbye from Interface B");
+    }
 }
 
 class Test implements A, B {
+
     @Override
     public void message() {
-        // resolving the conflict
+        System.out.println("Resolving default method conflict:");
+        A.super.message();
+        B.super.message();
+        System.out.println("Custom implementation in Test");
+    }
+
+    public void showInterfaceMessages() {
+        System.out.println("\nCalling individual interface methods:");
         A.super.message();
         B.super.message();
     }
@@ -21,7 +37,15 @@ class Test implements A, B {
 
 public class InterfaceConflictExample {
     public static void main(String[] args) {
-        Test t = new Test();
-        t.message();
+        Test obj = new Test();
+
+        System.out.println("=== Default Method Conflict Resolution ===");
+        obj.message();
+
+        System.out.println("\n=== Other Default Methods ===");
+        obj.greet();
+        obj.goodbye();
+
+        obj.showInterfaceMessages();
     }
 }
