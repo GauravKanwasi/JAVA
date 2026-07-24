0be6e9b0fbd9479ea2b288b8eb28f161277c9fb4
Gaurav Singh <kanwasigaurav96@gmail.com>
2026-07-24 22:43:24 +0530

    Add constructors and enhance display method in Dog class
    Added constructors to Animal and Dog classes, and modified display method to include additional print statements.

--- TestSuperMethod.java
+++ TestSuperMethod.java
@@ -1,20 +1,28 @@
-
 class Animal {
+    Animal() {
+        System.out.println("Animal constructor called");
+    }
+
     void sound() {
         System.out.println("Animals make sounds");
     }
 }
 
 class Dog extends Animal {
+    Dog() {
+        System.out.println("Dog constructor called");
+    }
+
+    @Override
     void sound() {
         System.out.println("Dogs bark");
     }
 
     void display() {
-
+        System.out.println("\nCalling parent class method:");
         super.sound();
 
-
+        System.out.println("\nCalling child class method:");
         sound();
     }
 }
@@ -22,6 +30,7 @@ class Dog extends Animal {
 public class TestSuperMethod {
     public static void main(String[] args) {
         Dog d = new Dog();
+        System.out.println();
         d.display();
     }
 }
