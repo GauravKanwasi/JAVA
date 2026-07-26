57e76d075c26f8ea55c87a63225c4bac58c692e5
Gaurav Singh <kanwasigaurav96@gmail.com>
2026-07-26 21:51:50 +0530

    Implement swap method for array elements using XOR
    Updated the swap logic to use a dedicated method for improved clarity and reusability.

--- SwapUsingXOR.java
+++ SwapUsingXOR.java
@@ -1,12 +1,18 @@
-class SwapUsingXOR {
+public class SwapUsingXOR {
+    static void swap(int[] arr, int i, int j) {
+        if (i == j) return;
+        arr[i] ^= arr[j];
+        arr[j] ^= arr[i];
+        arr[i] ^= arr[j];
+    }
     public static void main(String[] args) {
-        int a = 5, b = 10;
-
-        a = a ^ b;
-        b = a ^ b;
-        a = a ^ b;
-
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        int[] values = {5, 10};
+        System.out.println("Before Swap:");
+        System.out.println("a = " + values[0]);
+        System.out.println("b = " + values[1]);
+        swap(values, 0, 1);
+        System.out.println("\nAfter Swap:");
+        System.out.println("a = " + values[0]);
+        System.out.println("b = " + values[1]);
     }
 }
