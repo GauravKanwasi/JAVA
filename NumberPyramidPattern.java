fb264fe725e51ebac81dbf4884e6d936f885697f
Gaurav Singh <kanwasigaurav96@gmail.com>
2026-07-28 21:56:31 +0530

    Refactor Number Pyramid Pattern for user input
    Refactored the main method to use a separate method for printing the number pyramid. Added user input for the number of rows.

--- NumberPyramidPattern.java
+++ NumberPyramidPattern.java
@@ -1,20 +1,38 @@
+import java.util.Scanner;
+
 public class NumberPyramidPattern {
-    public static void main(String[] args) {
-        int n = 5; 
-        int number = 1; 
 
-        for (int i = 1; i <= n; i++) {
+    public static void printNumberPyramid(int rows) {
+        int number = 1;
 
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print("  "); 
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print("   ");
             }
 
-            for (int k = 1; k <= i; k++) {
-                System.out.printf("%3d", number); 
-                number++; 
+            for (int j = 1; j <= i; j++) {
+                System.out.printf("%3d", number++);
             }
-            
+
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+
+        if (rows <= 0) {
+            System.out.println("Please enter a positive number.");
+            return;
+        }
+
+        System.out.println("\nNumber Pyramid:\n");
+        printNumberPyramid(rows);
+
+        scanner.close();
     }
 }
