a2f0efb29f51a40e52203fa323928f19542d7e3d
Gaurav Singh <kanwasigaurav96@gmail.com>
2026-08-03 21:10:17 +0530

    Replace 3D array with stone game III implementation
    Refactored code to implement the stone game III solution while removing the original 3D array functionality.

--- ThreeDArray.java
+++ ThreeDArray.java
@@ -1,24 +1,31 @@
-class ThreeDArray {
-    public static void main(String[] args) {
-        int[][][] arr = {
-            {
-                {1, 2, 3},
-                {4, 5, 6}
-            },
-            {
-                {7, 8, 9},
-                {10, 11, 12}
-            }
-        };
+class Solution {
+public:
+    string stoneGameIII(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+        int dp1 = 0, dp2 = 0, dp3 = 0, cur;
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                for (int k = 0; k < arr[i][j].length; k++) {
-                    System.out.print(arr[i][j][k] + " ");
-                }
-                System.out.println();
+        for (int i = n - 1; i >= 0; --i) {
+            cur = INT_MIN;
+            int take = 0;
+
+            take += stoneValue[i];
+            cur = max(cur, take - dp1);
+
+            if (i + 1 < n) {
+                take += stoneValue[i + 1];
+                cur = max(cur, take - dp2);
             }
-            System.out.println();
+
+            if (i + 2 < n) {
+                take += stoneValue[i + 2];
+                cur = max(cur, take - dp3);
+            }
+
+            dp3 = dp2;
+            dp2 = dp1;
+            dp1 = cur;
         }
+
+        return dp1 > 0 ? "Alice" : dp1 < 0 ? "Bob" : "Tie";
     }
-}
+};
