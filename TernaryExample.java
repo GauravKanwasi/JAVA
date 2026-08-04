ccd1bdcaadf1a3f61df1851589f466b9f1f1ba06
Gaurav Singh <kanwasigaurav96@gmail.com>
2026-08-04 22:17:26 +0530

    Refactor TernaryExample to accept user input

--- TernaryExample.java
+++ TernaryExample.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class TernaryExample {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int max = (a > b) ? a : b;
-        System.out.println("The larger number is " + max);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int b = scanner.nextInt();
+
+        String result = (a > b)
+                ? "The larger number is " + a
+                : (a < b)
+                    ? "The larger number is " + b
+                    : "Both numbers are equal.";
+
+        System.out.println(result);
+
+        scanner.close();
     }
 }
