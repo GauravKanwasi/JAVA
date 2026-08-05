b5205a878f29cdce559215001a174c991824d858
Gaurav Singh <kanwasigaurav96@gmail.com>
2026-08-05 22:29:30 +0530

    Refactor Person and Student constructors with age and school

--- TestSuperConstructor.java
+++ TestSuperConstructor.java
@@ -1,31 +1,59 @@
-
 class Person {
-    String name;
+    private String name;
+    private int age;
 
-    Person(String name) {
+    Person(String name, int age) {
         this.name = name;
+        this.age = age;
         System.out.println("Person constructor called");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name : " + name);
+        System.out.println("Age  : " + age);
     }
 }
 
 class Student extends Person {
-    int grade;
+    private int grade;
+    private String school;
 
-    Student(String name, int grade) {
-
-        super(name);
+    Student(String name, int age, int grade, String school) {
+        super(name, age);
         this.grade = grade;
+        this.school = school;
         System.out.println("Student constructor called");
     }
 
-    void showDetails() {
-        System.out.println("Name: " + name + ", Grade: " + grade);
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Grade : " + grade);
+        System.out.println("School: " + school);
+    }
+
+    public void study() {
+        System.out.println(getName() + " is studying...");
     }
 }
 
 public class TestSuperConstructor {
     public static void main(String[] args) {
-        Student s = new Student("Gaurav", 10);
-        s.showDetails();
+
+        Student s = new Student("Gaurav", 16, 10, "ABC Public School");
+
+        System.out.println("\n--- Student Details ---");
+        s.displayInfo();
+
+        System.out.println();
+        s.study();
     }
 }
