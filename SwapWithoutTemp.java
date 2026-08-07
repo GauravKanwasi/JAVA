a630eb38b7938bc98febafec3b98c6db00e31071
Gaurav Singh <kanwasigaurav96@gmail.com>
2026-08-07 22:26:57 +0530

    Refactor swap logic to use bitwise XOR
    Updated the swapping logic to use bitwise XOR instead of arithmetic operations, and added print statements for clarity.

--- SwapWithoutTemp.java
+++ SwapWithoutTemp.java
@@ -1,12 +1,17 @@
-class SwapWithoutTemp {
+public class SwapWithoutTemp {
     public static void main(String[] args) {
-        int a = 5, b = 10;
+        int a = 5;
+        int b = 10;
 
-        a = a + b;  
-        b = a - b;  
-        a = a - b;
+        System.out.println("Before Swap:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
 
-        System.out.println("After Swap:");
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        System.out.println("\nAfter Swap:");
         System.out.println("a = " + a);
         System.out.println("b = " + b);
     }
